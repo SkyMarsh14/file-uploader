697632245928f46922223ed9b57e3470e5008d5c
Sky Randy <soras1758@gmail.com>
2025-04-04 15:51:16 +0900

    add backend form validation to login forms

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -22,6 +22,18 @@ const validateUser = [
     .isLength(passwordLength)
     .withMessage(lengthErr("password", passwordLength)),
 ];
+const validateConfirmationPassword = [
+  body("confirmPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("Password confirmation is required.")
+    .custom((value, { req }) => {
+      if (value === req.body.password) true;
+      else {
+        throw new Error("Passwords do not match.");
+      }
+    }),
+];
 const loginController = {
   sign_in_get: async (req, res) => {
     res.render("home", {
@@ -31,9 +43,13 @@ const loginController = {
       passwordLength,
     });
   },
-  sign_in_post: async (req, res) => {
-    res.redirect("/sign-in");
-  },
+  sign_in_post: [
+    validateUser,
+    async (req, res) => {
+      const error = validationResult(req);
+      res.redirect("/sign-in");
+    },
+  ],
   sign_up_get: async (req, res) => {
     res.render("home", {
       page: "login",
@@ -42,7 +58,14 @@ const loginController = {
       passwordLength,
     });
   },
-  sign_up_post: async (req, res) => {},
+  sign_up_post: [
+    validateUser,
+    validateConfirmationPassword,
+    async (req, res) => {
+      validationResult(req);
+      res.redirect("/sign-up");
+    },
+  ],
 };
 
 export default loginController;
